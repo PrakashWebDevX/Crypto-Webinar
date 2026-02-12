91f6d14cc9bdc7347bcaca6b003935aac96dabac
PrakashDevX <Prakashclever43@gmail.com>
2026-02-12 23:24:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,15 +76,14 @@ app.post('/register', async (req, res) => {
     // ===================================================
     if (email && process.env.EMAIL_USER && process.env.EMAIL_PASS) {
 
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 
       transporter.sendMail({
         from: process.env.EMAIL_USER,
